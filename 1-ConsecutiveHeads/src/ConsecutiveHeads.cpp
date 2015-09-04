af6ab7c54c4e2fcc8c6b10f0f87f7929f7d16921
shpp-vdotsenko <shpp-vdotsenko@programming.kr.ua>
2015-09-04 14:54:18 +0300

    Update ConsecutiveHeads.cpp

--- 1-ConsecutiveHeads/src/ConsecutiveHeads.cpp
+++ 1-ConsecutiveHeads/src/ConsecutiveHeads.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 #include <string>
-#include "console.h"
-#include "random.h"
+#include <stdlib.h>
+#include <time.h>
+
 using namespace std;
 
-int main() {
-   // TODO: fill in the code
-   return 0;
+int main () {
+	int	coinDrop = 0,		// code of coin side
+		heads = 0;			// number of heads, dropped one by one
+
+	srand (time(NULL));		// initialize random seed:
+
+	/*
+		0 - heads
+		1 - tails
+	*/
+
+	// continue to drop the coin, while head won't be achived 3 times in a row
+	while (heads < 3) {
+		coinDrop = rand() % 2;			// calculetes random drop
+		if (coinDrop == 1) {			// if tailes
+			heads = 0;
+			cout << "tails" << endl;
+		} else {						// if heads
+			heads++;
+			cout << "heads" << endl;
+		}
+	}
+	
+	return 0;
 }
